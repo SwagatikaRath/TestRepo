71ced34794600ab41dd0b5639fe1bcb74332cefb
Swagatika <reetu.swagatika@gmail.com>
2016-02-25 09:58:16 -0500

    Initial Commit

---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Swagatika on 2/25/16.
+//  Copyright © 2016 Swagatika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorld/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by Swagatika on 2/25/16.
+//  Copyright © 2016 Swagatika. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
